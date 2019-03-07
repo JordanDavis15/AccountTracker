bd5a96c9907e3c4d1b0436be93e1b784e12a9cac
JordanDavis15 <46542224+JordanDavis15@users.noreply.github.com>
2019-03-06 23:19:54 -0500

    fixed constructor and added amt var to Account class

--- src/account_tracker/Account.java
+++ src/account_tracker/Account.java
@@ -7,15 +7,17 @@ package account_tracker;
 
 public class Account {
     private String name;
-    private int num;
+    private int num, amt;
     
-    public Account(String name, int num){
-        this.name = name;
+    public Account(int num, String name, int amt){
         this.num = num;
+        this.name = name;
+        this.amt = amt;
+        
     }
     @Override
     public String toString(){
-        return name + "/" + num + "/";
+        return name + "/" + num + "/" + amt + "/";
     }
     
     
